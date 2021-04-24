ec447f431b5ded8df049b509f1282912434bb6c2
Mehru <mehrunnisa533@gmail.com>
2021-04-24 17:27:07 +0500

    Toggle button functionality added

--- flutter_assignment_4/lib/main.dart
+++ flutter_assignment_4/lib/main.dart
@@ -1,3 +1,8 @@
+// import 'dart:html';
+
+// import 'dart:html';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,10 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: '×       Sort & Filter       Clear       Apply'),
+      home: MyHomePage(),
     );
   }
 }
@@ -28,39 +33,145 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  String _selectedValue;
+  Map<String, bool> items = {
+    'Sale': false,
+    'Credit': false,
+    'Refund': false,
+    'Loan': false,
+    'Direct Material': false,
+    'Direct Labour': false,
+    'Asset': false,
+    'Other': false,
+    'Overhead': false,
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        title: Text('Sort & Filter'),
+        leading: Icon(
+          Icons.cancel,
+        ),
+        actions: [
+          TextButton(
+            child: Text(
+              'CLEAR',
+              style: TextStyle(color: Colors.white, fontSize: 18),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            onPressed: () {},
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          TextButton(
+            child: Text(
+              'APPLY',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            onPressed: () {},
+          ),
+          SizedBox(
+            width: 6,
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Sort By',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+            DropdownButton<String>(
+              value: _selectedValue,
+              iconEnabledColor: Colors.purple,
+              underline: Container(
+                height: 2,
+                color: Colors.purple,
+              ),
+              isExpanded: true,
+              items: <String>[
+                "Newest first",
+                "Oldest first",
+                "Value High-Low",
+                "Value Low-High",
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                    value,
+                    style: TextStyle(color: Colors.black),
+                  ),
+                );
+              }).toList(),
+              hint: Text(
+                ('Choose an option'),
+                style: TextStyle(fontSize: 13),
+              ),
+              onChanged: (String value) {
+                setState(() {
+                  _selectedValue = value;
+                });
+              },
+            ),
+            SizedBox(height: 15),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Transaction Types',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 14),
+            ToggleButtons(
+              borderRadius: BorderRadius.circular(32.0),
+              fillColor: Colors.purple,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text(
+                    'Money in',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text(
+                    'Money out',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+              onPressed: (i) {},
+              isSelected: [false, true],
+            ),
+            SizedBox(height: 3),
+            Expanded(
+              child: ListView(
+                children: items.keys.map((String key) {
+                  return new CheckboxListTile(
+                    title: new Text(key),
+                    value: items[key],
+                    activeColor: Colors.purple,
+                    checkColor: Colors.white,
+                    onChanged: (bool value) {
+                      setState(() {
+                        items[key] = value;
+                      });
+                    },
+                  );
+                }).toList(),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
